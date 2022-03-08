a9609338f6e43c082c702585fb3dbcf5a84be84f
Javier Rojas <javier4le.dev@gmail.com>
2022-03-08 02:04:17 -0300

    ADD: Basic Project Structure

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/* =============== CLOCK =============== */
+
+
+	// We add a rotation to the elements
+
+
+/* =============== CLOCK & DATE TEXT =============== */
+
+	// We change the hours from 24 to 12 hours and establish whether it is AM or PM
+
+
+	// We detext when it's 0 AM and tranform to 12 AM
+
+
+	// Show a zero before hours
+
+
+	// Show time
+
+
+	// Show a zero before the minutes
+
+
+	// Show minutes
+
+
+	// Show am or pm
+
+
+	// We get the months of the year and show it
+
+
+	// We show the day, the month and the year
+
+
+/* =============== DARK/LIGHT THEME =============== */
